877ffddeee48d6bc51a6a8ed4b754195ff274d31
SilnkD <iamsilenkova@gmail.com>
2025-02-16 22:06:51 +0300

    feat swagger

--- finance-app/src/users/dto/create-user-dto.ts
+++ finance-app/src/users/dto/create-user-dto.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateUserDto {
+    @ApiProperty({example: 'example@gmail.com', description: 'Электронная почта'})
     readonly email: string;
+
+    @ApiProperty({example: 'password123', description: 'Пароль'})
     readonly password: string;
 }
--- finance-app/src/users/users.model.ts
+++ finance-app/src/users/users.model.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { AllowNull, AutoIncrement, Column, DataType, Model, PrimaryKey, Table } from "sequelize-typescript";
 
 interface UserCreationAttrs {
@@ -7,10 +8,15 @@ interface UserCreationAttrs {
 
 @Table({tableName:'users'})
 export class User extends Model <User, UserCreationAttrs> {
+    @ApiProperty({example: '1', description: 'Уникальный идентификатор'})
     @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
     id:number;
+
+    @ApiProperty({example: 'example@gmail.com', description: 'Электронная почта'})
     @Column({type: DataType.STRING, unique: true, allowNull: false})
     email: string;
+
+    @ApiProperty({example: 'password123', description: 'Пароль'})
     @Column({type: DataType.STRING, allowNull: false})
     password: string;
 }
