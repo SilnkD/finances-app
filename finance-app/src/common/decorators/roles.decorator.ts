7b974b7e297f100380c42f3869bd226ffa5f681d
SilnkD <iamsilenkova@gmail.com>
2025-02-23 12:11:18 +0300

    fixing

--- finance-app/src/common/decorators/roles.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { Role } from 'src/roles/roles.enum';
-
-export const Roles = (...roles: Role[]) => SetMetadata('roles', roles);
-//@Roles(Role.Admin) используется в контроллере для ограничения доступа по роли пользователя
